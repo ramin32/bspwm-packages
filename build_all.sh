d1aa76ab7892ab81198ee520beaa381c2e29a5ce
Ramin Rakhamimov <ramin32@gmail.com>
2014-03-28 14:17:28 -0400

    added build commands

--- build_all.sh
+++ build_all.sh
@@ -1,1 +1,6 @@
 sudo apt-get install xcb libxcb-util0-dev libxcb-ewmh1-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev
+cd bar && make && && sudo make install ; cd ..
+cd sutils && make && sudo make install ; cd ..
+cd xtitle && make && sudo make install ; cd ..
+cd sxhkd && make && sudo make install ; cd ..
+cd bspwm && make && sudo make install ; cd ..
